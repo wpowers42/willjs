1697f08f396baf723f1985177291d4598495f6c2
Will Powers <wpowers@classdojo.com>
2022-11-12 20:40:56 -0700

    boilerplate for animations on trigger

---
+++ animations-on-trigger/script.js
@@ -0,0 +1,11 @@
+const canvas = /** @type {HTMLCanvasElement} */ (document.getElementById('canvas'));
+const ctx = canvas.getContext('2d');
+const CANVAS_WIDTH = canvas.width = 600;
+const CANVAS_HEIGHT = canvas.height = 600;
+
+function animate() {
+    ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+    requestAnimationFrame(animate);
+}
+
+animate();
