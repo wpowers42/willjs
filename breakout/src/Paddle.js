95035ff30e21015d2b923ce431cf34c9da13d917
Will Powers <wpowers@classdojo.com>
2022-12-04 20:16:45 -0700

    add paddle class

--- breakout/src/Paddle.js
+++ breakout/src/Paddle.js
@@ -1,1 +1,36 @@
+import { Mathf } from "../../math/Mathf.js";
+import { Constants } from "./constants.js";
+export default class Paddle {
+    constructor() {
+        this.width = 64;
+        this.height = 16;
+        this.x = Constants.canvasWidth * 0.50 - this.width * 0.50;
+        this.y = Constants.canvasHeight - 32;
+        this.dx = 0;
+        this.skin = 0;
+        this.size = 2;
+    }
+    update(dt, inputHandler) {
+        if (inputHandler.isKeyPressed('ArrowLeft')) {
+            this.dx = -Constants.paddleSpeed;
+        }
+        else if (inputHandler.isKeyPressed('ArrowRight')) {
+            this.dx = Constants.paddleSpeed;
+        }
+        else {
+            this.dx = 0;
+        }
+        this.x = this.dx * dt;
+        this.x = Mathf.Clamp(this.x, 0, Constants.canvasWidth);
+    }
+    draw(ctx) {
+        let quad = Constants.frames.paddles[this.skin * 4 + this.size];
+        let { sx, sy, sw, sh } = quad;
+        let dx = this.x;
+        let dy = this.y;
+        let dw = this.width;
+        let dh = this.height;
+        ctx.drawImage(Constants.textures.main, sx, sy, sw, sh, dx, dy, dw, dh);
+    }
+}
 //# sourceMappingURL=Paddle.js.map
---
+++ breakout/src/Paddle.ts
@@ -0,0 +1,47 @@
+import { Mathf } from "../../math/Mathf.js";
+import { Constants } from "./constants.js";
+import InputHandler from "./InputHandler";
+
+export default class Paddle {
+    width: number;
+    height: number;
+    x: number;
+    y: number;
+    dx: number;
+    skin: number;
+    size: number;
+
+    constructor() {
+        this.width = 64;
+        this.height = 16;
+        this.x = Constants.canvasWidth * 0.50 - this.width * 0.50;
+        this.y = Constants.canvasHeight - 32;
+        this.dx = 0;
+        this.skin = 0;
+        this.size = 2;
+    }
+
+    update(dt: number, inputHandler: InputHandler) {
+        if (inputHandler.isKeyPressed('ArrowLeft')) {
+            this.dx = -Constants.paddleSpeed;
+        } else if (inputHandler.isKeyPressed('ArrowRight')) {
+            this.dx = Constants.paddleSpeed;
+        } else {
+            this.dx = 0;
+        }
+
+        this.x = this.dx * dt;
+        this.x = Mathf.Clamp(this.x, 0, Constants.canvasWidth);
+    }
+
+    draw(ctx: CanvasRenderingContext2D) {
+        let quad = Constants.frames.paddles[this.skin * 4 + this.size];
+        let { sx, sy, sw, sh } = quad;
+        let dx = this.x;
+        let dy = this.y;
+        let dw = this.width;
+        let dh = this.height;
+        ctx.drawImage(Constants.textures.main, sx, sy, sw, sh, dx, dy, dw, dh);
+    }
+
+}
