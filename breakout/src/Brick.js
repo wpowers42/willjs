e4cf4202522cc7adceefa4d09c118a036ec66bf2
Will Powers <wpowers@classdojo.com>
2022-12-04 21:44:35 -0700

    add brick class

--- breakout/src/Brick.js
+++ breakout/src/Brick.js
@@ -1,1 +1,31 @@
+import { Constants } from "./constants.js";
+export default class Brick {
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
+        this.width = 32;
+        this.height = 16;
+        this.tier = 0;
+        this.color = 1;
+        this.inPlay = true;
+    }
+    hit() {
+        Constants.sounds.brickHit2.play();
+        this.inPlay = false;
+    }
+    draw(ctx) {
+        if (this.inPlay) {
+            let quad = Constants.frames.bricks[this.color * 4 + this.tier];
+            let { sx, sy, sw, sh } = quad;
+            let dx = this.x;
+            let dy = this.y;
+            let dw = this.width;
+            let dh = this.height;
+            ctx.drawImage(Constants.textures.main, sx, sy, sw, sh, dx, dy, dw, dh);
+            if (Constants.debug) {
+                ctx.strokeRect(this.x, this.y, this.width, this.height);
+            }
+        }
+    }
+}
 //# sourceMappingURL=Brick.js.map
---
+++ breakout/src/Brick.ts
@@ -0,0 +1,41 @@
+import { Constants } from "./constants.js";
+
+export default class Brick {
+    x: number;
+    y: number;
+    width: number;
+    height: number;
+    tier: number;
+    color: number;
+    inPlay: boolean;
+
+    constructor(x: number, y: number) {
+        this.x = x;
+        this.y = y;
+        this.width = 32;
+        this.height = 16;
+        this.tier = 0;
+        this.color = 1;
+        this.inPlay = true;
+    }
+
+    hit() {
+        Constants.sounds.brickHit2.play();
+        this.inPlay = false;
+    }
+
+    draw(ctx: CanvasRenderingContext2D) {
+        if (this.inPlay) {
+            let quad = Constants.frames.bricks[this.color * 4 + this.tier];
+            let { sx, sy, sw, sh } = quad;
+            let dx = this.x;
+            let dy = this.y;
+            let dw = this.width;
+            let dh = this.height;
+            ctx.drawImage(Constants.textures.main, sx, sy, sw, sh, dx, dy, dw, dh);
+            if (Constants.debug) {
+                ctx.strokeRect(this.x, this.y, this.width, this.height);
+            }
+        }
+    }
+}
