a5e0f819099ac732d5fc816a2360b6438ef4d6a7
Will Powers <wpowers@classdojo.com>
2022-12-07 20:59:48 -0700

    add colors and tiers to level maker

--- breakout/src/LevelMaker.js
+++ breakout/src/LevelMaker.js
@@ -1,19 +1,61 @@
+import { Mathf } from "../../math/Mathf.js";
 import Brick from "./Brick.js";
 export default class LevelMaker {
     constructor() {
     }
 }
+// global patterns
+LevelMaker.none = 1;
+LevelMaker.single_pyramid = 2;
+LevelMaker.multi_pyramid = 3;
+// per-row patterns
+LevelMaker.solid = 1; // all colors the same in this row
+LevelMaker.alternate = 2; // alternate colors
+LevelMaker.skip = 3; // skip every other block
+LevelMaker._none = 4; // no blocks this row
 LevelMaker.createMap = (level) => {
-    let bricks = [];
-    let numRows = Math.floor(Math.random() * 4 + 1);
-    let numCols = Math.floor(Math.random() * 6 + 7);
+    const bricks = [];
+    const numRows = Mathf.RandomInt(1, 5);
+    let numCols = Mathf.RandomInt(7, 13);
+    numCols = numCols % 2 === 0 ? numCols + 1 : numCols; // ensure odd
+    const highestTier = Math.min(3, Math.floor(level / 5));
+    const highestColor = Math.min(5, level % 5 + 3);
     for (let y = 0; y < numRows; y++) {
+        // whether we want to enable skipping for this row
+        const skipPattern = Math.random() < 0.50;
+        // whether we want alternate colors for this row
+        const alternatePattern = Math.random() < 0.50;
+        const alternateColor1 = Mathf.RandomInt(1, highestColor);
+        const alternateColor2 = Mathf.RandomInt(1, highestColor);
+        const alternateTier1 = Mathf.RandomInt(0, highestTier);
+        const alternateTier2 = Mathf.RandomInt(0, highestTier);
+        let skipFlag = Math.random() < 0.50;
+        let alternateFlag = Math.random() < 0.50;
+        const solidColor = Mathf.RandomInt(1, highestColor);
+        const solidTier = Mathf.RandomInt(0, highestTier);
         for (let x = 0; x < numCols; x++) {
-            let brick = new Brick(x * 32 // brick width times index
+            skipFlag = !skipFlag; // flip the flag
+            if (skipPattern && skipFlag) {
+                continue;
+            }
+            const brick = new Brick(x * 32 // brick width times index
                 + 8 // 8px for the screen
                 + (13 - numCols) * 16, // math to center entire row
             y * 16 + 16 // brick height times index
             );
+            alternateFlag = !alternateFlag;
+            if (alternatePattern && alternateFlag) {
+                brick.color = alternateColor1;
+                brick.tier = alternateTier1;
+            }
+            else {
+                brick.color = alternateColor2;
+                brick.tier = alternateTier2;
+            }
+            if (!alternatePattern) {
+                brick.color = solidColor;
+                brick.tier = solidTier;
+            }
             bricks.push(brick);
         }
     }
--- breakout/src/LevelMaker.ts
+++ breakout/src/LevelMaker.ts
@@ -1,24 +1,80 @@
+import { Mathf } from "../../math/Mathf.js";
 import Brick from "./Brick.js";
 
 export default class LevelMaker {
+
+    // global patterns
+    static none = 1;
+    static single_pyramid = 2;
+    static multi_pyramid = 3;
+
+    // per-row patterns
+    static solid = 1; // all colors the same in this row
+    static alternate = 2; // alternate colors
+    static skip = 3; // skip every other block
+    static _none = 4; // no blocks this row
+    
+
     constructor() {
 
     }
 
     static createMap = (level: number): Brick[] => {
-        let bricks = [];
+        const bricks = [];
 
-        let numRows = Math.floor(Math.random() * 4 + 1);
-        let numCols = Math.floor(Math.random() * 6 + 7);
+        const numRows = Mathf.RandomInt(1, 5);
+        let numCols = Mathf.RandomInt(7, 13);
+        numCols = numCols % 2 === 0 ? numCols + 1 : numCols; // ensure odd
+        
+        const highestTier = Math.min(3, Math.floor(level / 5));
+        const highestColor = Math.min(5, level % 5 + 3);
 
+        
         for (let y = 0; y < numRows; y++) {
+            // whether we want to enable skipping for this row
+            const skipPattern = Math.random() < 0.50;
+            
+            // whether we want alternate colors for this row
+            const alternatePattern = Math.random() < 0.50;
+            
+            const alternateColor1 = Mathf.RandomInt(1, highestColor);
+            const alternateColor2 = Mathf.RandomInt(1, highestColor);
+            const alternateTier1 = Mathf.RandomInt(0, highestTier);
+            const alternateTier2 = Mathf.RandomInt(0, highestTier);
+            
+            let skipFlag = Math.random() < 0.50;
+            let alternateFlag = Math.random() < 0.50;
+            
+            const solidColor = Mathf.RandomInt(1, highestColor);
+            const solidTier = Mathf.RandomInt(0, highestTier);
+            
             for (let x = 0; x < numCols; x++) {
-                let brick = new Brick(
+                skipFlag = !skipFlag; // flip the flag
+                if (skipPattern && skipFlag) {    
+                    continue;
+                }
+                
+                const brick = new Brick(
                     x * 32                 // brick width times index
                     + 8                    // 8px for the screen
                     + (13 - numCols) * 16, // math to center entire row
                     y * 16 + 16            // brick height times index
                 );
+
+                alternateFlag = !alternateFlag;
+                if (alternatePattern && alternateFlag) {
+                    brick.color = alternateColor1;
+                    brick.tier = alternateTier1;
+                } else {
+                    brick.color = alternateColor2;
+                    brick.tier = alternateTier2;
+                }
+
+                if (!alternatePattern) {
+                    brick.color = solidColor;
+                    brick.tier = solidTier;
+                }
+
                 bricks.push(brick);
             }
         }
