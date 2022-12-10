4748c4586225496b37cc3dee7a752523e4317d9c
Will Powers <wpowers@classdojo.com>
2022-12-09 22:02:38 -0700

    expand Mathf methods

--- games/math/Mathf.js
+++ games/math/Mathf.js
@@ -20,11 +20,34 @@ export var Mathf;
             return value;
         }
     };
+    // Interpolates between /a/ and /b/ by /t/. /t/ is clamped between 0 and 1.
+    Mathf.Lerp = (a, b, t) => {
+        return a + (b - a) * Mathf.Clamp01(t);
+    };
     Mathf.Deg2Rad = Math.PI * 2 / 360;
     Mathf.Rad2Deg = 1.0 / Mathf.Deg2Rad;
     // Custom Implementation
+    Mathf.RandomRange = (minValue, maxValue) => {
+        return Math.random() * (maxValue - minValue) + minValue;
+    };
     Mathf.RandomInt = (minValue, maxValue) => {
-        return Math.floor(Math.random() * (maxValue - minValue + 1)) + minValue;
+        return Math.floor(Mathf.RandomRange(minValue, maxValue));
+    };
+    Mathf.RandomNormal = (mean, standardDeviation) => {
+        // Generate two random numbers between 0 and 1
+        const u = 1 - Math.random();
+        const v = Math.random();
+        // Use the Box-Muller transform to convert the random numbers
+        // into a normal distribution with the given mean and standard deviation
+        const z = Math.sqrt(-2 * Math.log(u)) * Math.cos(2 * Math.PI * v);
+        return mean + standardDeviation * z;
+    };
+    Mathf.LerpList = (start, end, t) => {
+        // Check that the two lists have the same length
+        if (start.length !== end.length) {
+            throw new Error('Lists have different lengths');
+        }
+        return start.map((x, i) => Mathf.Lerp(x, end[i], t));
     };
 })(Mathf || (Mathf = {}));
 //# sourceMappingURL=Mathf.js.map
