4c5834697ff8c615d6ad4d826f2dadedb9a0c215
Will Powers <wpowers@classdojo.com>
2024-11-10 22:08:03 +0000

    add prior sandbox update

--- games/sandbox-3d/script.js
+++ games/sandbox-3d/script.js
@@ -1,10 +1,27 @@
-const events = class5f3dea1f0f16134ac236cd1e;
+// import class-657abe546f2f0f0ee0c59114.json without using require
+fetch('./class-657abe546f2f0f0ee0c59114.json')
+    .then(response => {
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
+        }
+        return response.json();
+    })
+    .then(data => {
+        data = data.filter(event => event.entity_id == '65AE9272D613208BEFAB5B2B00000000')
+        main(data);
+
+    })
+    .catch(error => {
+        console.error('There has been a problem with your fetch operation:', error);
+    });
+
+// console.log(events);
 
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 500);
 
-const renderer = new THREE.WebGLRenderer({alpha: true});
+const renderer = new THREE.WebGLRenderer({ alpha: true });
 
 renderer.setSize(window.innerWidth, window.innerHeight);
 
@@ -17,31 +34,32 @@ camera.position.set(-45, 10, 73);
 
 
 const controls = new THREE.OrbitControls(camera, renderer.domElement);
-controls.target.set( -12.5, 1, 45 );
-controls.maxZoom = 1.0;
+// controls.target.set(-12.5, 1, 45);
+controls.maxZoom = 2.0;
 controls.maxDistance = 60.0;
-controls.enablePan = false;
+controls.enablePan = true; // false normally
 controls.enableKeys = false;
 controls.maxPolarAngle = Math.PI / 2;
-controls.autoRotate = true;
+controls.autoRotate = false; // true to rotate
+
 
 const colorMap = {
-      "BuildableBlockPurple": 0x9478DA,
-      "BuildableBlockBlue": 0x71BCD5,
-      "BuildableBlockYellow": 0xE9D47D,
-      "BuildableBlockRed": 0xD15D58,
-      "SandboxGround": 0xF5D3AB,
-      "Water": 0xA0D3E3
+    "BuildableBlockPurple": 0x9478DA,
+    "BuildableBlockBlue": 0x71BCD5,
+    "BuildableBlockYellow": 0xE9D47D,
+    "BuildableBlockRed": 0xD15D58,
+    "SandboxGround": 0xF5D3AB,
+    "Water": 0xA0D3E3
 }
 
-renderer.setClearColor( colorMap.Water, 1);
+renderer.setClearColor(colorMap.Water, 1);
 
 const sandboxCoordinates = {
     p1: (-45, 1, 17),
     p2: (20, 1, 17),
     p3: (20, 1, 73),
     p4: (-45, 1, 73)
-    
+
 }
 
 
@@ -52,20 +70,20 @@ const sandboxCoordinates = {
 const g = new THREE.BufferGeometry();
 // create a simple square shape. We duplicate the top left and bottom right
 // vertices because each vertex needs to appear once per triangle.
-const v = new Float32Array( [
-	-45.5,  0.48,  17.5,
-    20.5,  0.48,  17.5,
-    -45.5,  0.48,  73.5,
+const v = new Float32Array([
+    -45.5, 0.48, 17.5,
+    20.5, 0.48, 17.5,
+    -45.5, 0.48, 73.5,
 
-    20.5,  0.48,  17.5,
-    20.5,  0.48,  73.5,
-	-45.5,  0.48,  73.5
-] );
+    20.5, 0.48, 17.5,
+    20.5, 0.48, 73.5,
+    -45.5, 0.48, 73.5
+]);
 
 // itemSize = 3 because there are 3 values (components) per vertex
-g.setAttribute( 'position', new THREE.BufferAttribute( v, 3 ) );
-const m = new THREE.MeshBasicMaterial( { color: colorMap.SandboxGround, side: THREE.DoubleSide } );
-const me = new THREE.Mesh( g, m );
+g.setAttribute('position', new THREE.BufferAttribute(v, 3));
+const m = new THREE.MeshBasicMaterial({ color: colorMap.SandboxGround, side: THREE.DoubleSide });
+const me = new THREE.Mesh(g, m);
 
 scene.add(me);
 
@@ -73,7 +91,9 @@ scene.add(me);
 let eventIndex = 0;
 let cubes = [];
 
-function handleCube() {
+// 65AE9272D613208BEFAB5B2B00000000
+
+function handleCube(events) {
     if (eventIndex >= events.length) {
         return;
     }
@@ -83,6 +103,8 @@ function handleCube() {
     let y = event.y;
     let z = event.z;
     let color = colorMap[event.block_type];
+
+    console.log(event.entity_id, event.event_at);
 
     if (event.event_type == 'block_destroy') {
         let blockToRemove = cubes.filter(cube => cube.position == (x, y, z))[0];
@@ -101,31 +123,38 @@ function handleCube() {
     eventIndex++;
 }
 
-let drawAll = false;
+const main = (events) => {
 
-if (drawAll) {
-    for (let i = 0; i < events.length; i++) {
-        handleCube();
+
+    let drawAll = false;
+
+    if (drawAll) {
+        for (let i = 0; i < events.length; i++) {
+            handleCube(events);
+        }
     }
+
+
+    animate(currentTime, events);
 }
 
 let currentTime = 0;
 let accumulator = 0;
-let timeBetweenCubes = 5;
+let timeBetweenCubes = 100;
 
-function animate(timestamp) {
+function animate(timestamp, events) {
 
     let deltatime = timestamp - currentTime;
     currentTime = timestamp;
     accumulator += deltatime;
 
     while (accumulator >= timeBetweenCubes) {
-        handleCube();
+        handleCube(events);
         accumulator -= timeBetweenCubes;
     }
 
     controls.update();
     renderer.render(scene, camera);
-    requestAnimationFrame(animate);
+    requestAnimationFrame((timestamp) => animate(timestamp, events));
 }
-animate(currentTime);
+
