fdc17f226bc753fb3c38ebd005d82a439ed5916f
Will Powers <wpowers@classdojo.com>
2024-11-28 04:57:14 +0000

    integrate mouse and touch support

--- nature-of-code/engine/input/InputManager.js
+++ nature-of-code/engine/input/InputManager.js
@@ -1,16 +1,19 @@
 // InputManager.js
 import { MouseListener } from './MouseListener.js';
 import { KeyboardListener } from './KeyboardListener.js';
+import { TouchListener } from './TouchListener.js';
 
 export class InputManager {
     constructor() {
         this.mouse = new MouseListener();
         this.keyboard = new KeyboardListener();
+        this.touch = new TouchListener();
     }
 
     initialize() {
         this.mouse.attachListeners();
         this.keyboard.attachListeners();
+        this.touch.attachListeners();
     }
 
     // Methods to retrieve input states
@@ -20,5 +23,17 @@ export class InputManager {
 
     getMousePosition() {
         return this.mouse.getPosition();
+    }
+
+    getTouchPosition() {
+        return this.touch.getTouchPosition();
+    }
+
+    getLatestMouseTouchPosition() {
+        if (this.mouse.getMousePositionUpdatedAt() > this.touch.getTouchPositionUpdatedAt()) {
+            return this.mouse.getPosition();
+        } else {
+            return this.touch.getTouchPosition();
+        }
     }
 }
