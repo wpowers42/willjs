dd7349512a779c20e0082c033e851ba042ffd797
Will Powers <wpowers@classdojo.com>
2024-11-29 06:00:15 +0000

    draw mover with stroke instead of fill

--- nature-of-code/engine/v2/core/Mover.js
+++ nature-of-code/engine/v2/core/Mover.js
@@ -35,7 +35,7 @@ export class Mover {
         ctx.fillStyle = "black";
         ctx.beginPath();
         ctx.arc(this.position.x, this.position.y, this.radius, 0, Math.PI * 2);
-        ctx.fill();
+        ctx.stroke();
     }
 
     checkEdges(width, height) {
