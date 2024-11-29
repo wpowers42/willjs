c29c696f21c312ef4ec7cea00b13bc68909f18a5
Will Powers <wpowers@classdojo.com>
2024-11-29 05:37:24 +0000

    update mover for forces

--- nature-of-code/engine/v2/core/Mover.js
+++ nature-of-code/engine/v2/core/Mover.js
@@ -3,11 +3,23 @@
 import { Vector } from "./Vector.js";
 
 export class Mover {
-    constructor(position, velocity, acceleration, maxSpeed) {
+    constructor(position, velocity, mass) {
         this.position = position;
         this.velocity = velocity || Vector.zero();
-        this.acceleration = acceleration || Vector.zero();
-        this.maxSpeed = maxSpeed || 0;
+        this.acceleration = Vector.zero();
+        this.maxSpeed = 0;
+        this.mass = mass || 1;
+        this.radius = mass * 5;
+    }
+
+    setMaxSpeed(maxSpeed) {
+        this.maxSpeed = maxSpeed;
+    }
+
+    applyForce(force) {
+        const forceVector = Vector.copy(force);
+        forceVector.div(this.mass);
+        this.acceleration.add(forceVector);
     }
 
     update() {
@@ -16,30 +28,35 @@ export class Mover {
         if (this.maxSpeed > 0) {
             this.velocity.limit(this.maxSpeed);
         }
+        this.acceleration.mult(0);
     }
 
     draw(ctx) {
         ctx.fillStyle = "black";
         ctx.beginPath();
-        ctx.arc(this.position.x, this.position.y, 10, 0, Math.PI * 2);
+        ctx.arc(this.position.x, this.position.y, this.radius, 0, Math.PI * 2);
         ctx.fill();
     }
 
     checkEdges(width, height) {
-        if (this.position.x > width) {
-            this.position.x = 0;
+        if (this.position.x + this.radius > width) {
+            this.position.x = width - this.radius;
+            this.velocity.x *= -1;
         }
 
-        if (this.position.x < 0) {
-            this.position.x = width;
+        if (this.position.x - this.radius < 0) {
+            this.position.x = this.radius;
+            this.velocity.x *= -1;
         }
 
-        if (this.position.y > height) {
-            this.position.y = 0;
+        if (this.position.y + this.radius > height) {
+            this.position.y = height - this.radius;
+            this.velocity.y *= -1;
         }
 
-        if (this.position.y < 0) {
-            this.position.y = height;
+        if (this.position.y - this.radius < 0) {
+            this.position.y = this.radius;
+            this.velocity.y *= -1;
         }
     }
 }
